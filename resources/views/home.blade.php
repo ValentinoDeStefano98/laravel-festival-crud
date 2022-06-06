78a295ffb8f71c9adbd9ff0edbab93bf2300a8f7
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-06-06 16:40:57 +0200

    database added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.layout')
 
-    <h1>ciao</h1>
-    
+@section('title')
+    Home
+@endsection
 
-    <script src=" {{ asset('js/app.js') }} "></script>
-</body>
-</html>
+@section('content')
+    <h2>Home</h2>
+@endsection
